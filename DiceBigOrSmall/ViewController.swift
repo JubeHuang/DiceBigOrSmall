505c292e623bad88dc41dfd3ab74248a872fdd05
Jube Huang <jube820106@gmail.com>
2022-09-26 18:42:28 +0800

    Initial Commit

---
+++ DiceBigOrSmall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceBigOrSmall
+//
+//  Created by JubeHuang黃冬伊 on 2022/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
